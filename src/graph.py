2b905f3501a4d6d7d2aedeaaf61ac24ecf4ddac1
bsmith578 <brettsmith578@gmail.com>
2024-04-29 16:49:08 -0500

    implemented revised graph class

--- src/graph.py
+++ src/graph.py
@@ -1,11 +1,12 @@
 from data_loader import DataLoader
 import matplotlib.pyplot as plt
+from pandas import isna
 
 # Class for Graphing Data
 class Graph:
     
     # Initialize with name of the Excel File
-    def __init__(self, file_name, start_year, end_year, msr):
+    def __init__(self, file_name):
         # Create instance of ExcelLoader with file name and load its data
         self.loader = DataLoader(file_name)
         self.loader.load_excel()
@@ -18,11 +19,6 @@ class Graph:
         # Call get_data method to store necessary data in lists
         self.get_data()
         
-        # Store indices for selected years and measure for later indexing
-        self.start_yr_index = self.years.index(start_year)
-        self.end_yr_index = self.years.index(end_year)
-        self.msr_index = self.measures.index(msr)
-        
     # Define a method for retrieving dataframes and measures from ExcelLoader
     def get_data(self):
         for sheet_name in self.loader.all_sheets:
@@ -31,8 +27,12 @@ class Graph:
         self.measures = list(self.all_dfs[0]['Measures'])
         
     # Define a method for graphing data onto a scatter plot
-    def plot_data(self):
+    def plot_data(self, start_year, end_year, msr):
         try:
+            # Store indices for selected years and measure for later indexing
+            self.start_yr_index = self.years.index(start_year)
+            self.end_yr_index = self.years.index(end_year)
+            self.msr_index = self.measures.index(msr)
             # Create a pyplot figure
             plt.figure(figsize=(10,8))
             # Declare a list to hold certain data that will be used for the y axis
@@ -40,8 +40,18 @@ class Graph:
             
             # Loop through selected dataframes
             for dfs in self.all_dfs[self.start_yr_index:(self.end_yr_index + 1)]:
-                # Append selected value from "% students met target" column to y_axis[]
-                y_axis.append(round((dfs['% students met target'][self.msr_index]) * 100))
+                # Check if value in '% students met target' is NaN
+                if isna(dfs['% students met target'][self.msr_index]):
+                    # If so, check if value in '#students meeting target' is NaN
+                    if isna(dfs['#students meeting target'][self.msr_index]):
+                        # If both values are missing append 0 to y_axis[]
+                        y_axis.append(0)
+                    else:
+                        # Calculate % if '#students meeting target' is present and append value to y_axis
+                        y_axis.append(round(((dfs['#students meeting target'][self.msr_index]) / (dfs['# students'][self.msr_index])) * 100))
+                else:
+                    # Append value to y_axis if no calculation is needed
+                    y_axis.append(round((dfs['% students met target'][self.msr_index]) * 100))
             
             # Graph data into a scatter plot w/ horizontal line representing target %
             plt.scatter(self.years[self.start_yr_index:(self.end_yr_index + 1)], y_axis)
@@ -56,10 +66,11 @@ class Graph:
             # Get current axes, set labels for X ticks, set Y axis limit
             ax = plt.gca()
             ax.set_xticklabels(self.years[self.start_yr_index:(self.end_yr_index + 1)])
-            ax.set_ylim([0,100])
+            ax.set_ylim([0,105])
+            plt.ylim(bottom = 1)
             
             # Rotate and align X tick labels and show horizontal grid
-            plt.xticks(rotation = 30, ha = 'center')
+            plt.xticks(rotation = 25, ha = 'center')
             plt.grid(axis = 'y')
             
             # Display graph
